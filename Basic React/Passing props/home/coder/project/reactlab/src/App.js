219d362e5004c7cf51a72dcfdee1327a02798d4d
Rachel <hurui.lcy@gmail.com>
2024-10-17 14:34:42 -0600

    Initial commit

---
+++ Basic React/Passing props/home/coder/project/reactlab/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName = "Rachel"/>
+            <Heading firstName = "Percy"/>
+        </div>
+    );
+};
+
+export default App;
